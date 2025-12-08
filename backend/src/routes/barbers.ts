1d1ce59e45d6dc1eff6caa414f8208cf7d10887e
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-08 17:27:06 +0100

    Add barbers route to handle fetching barbers from the service

---
+++ backend/src/routes/barbers.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import fetchBarbers from "../services/barberService.ts";
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const barbers = await fetchBarbers();
+    res.status(200).json(barbers);
+  } catch (error) {
+    res.status(500).json({ 
+      error: "Failed to fetch barbers",
+      message: error instanceof Error ? error.message : "Unknown error" 
+    });
+  }
+})
+
+export default router;
