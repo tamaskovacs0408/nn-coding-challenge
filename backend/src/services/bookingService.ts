6f91a9609b03d3f074b3d7964fff0f4635f93b49
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-09 09:04:22 +0100

    Update service structure and add new validation

--- backend/src/services/bookingService.ts
+++ backend/src/services/bookingService.ts
@@ -2,6 +2,8 @@ import { readFile, writeFile } from "fs/promises";
 import { randomUUID } from "crypto";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
+import fetchBarbers from "./barberService.ts";
+import type { WorkSchedule } from "./barberService.ts";
 
 interface Booking {
   id: string;
@@ -14,6 +16,8 @@ interface Booking {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const filePath = join(__dirname, "../data/bookings.json");
+
+const WEEKDAYS: (keyof WorkSchedule)[] = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 export async function loadBookings(): Promise<Booking[]> {
   try {
@@ -40,18 +44,29 @@ export async function saveBookings(bookings: Booking[]): Promise<void> {
 
 export async function createBooking(bookingData: {email: string, barberId: string, date: string, time: string}): Promise<Booking> {
   const bookings = await loadBookings();
-  const { email, barberId, date, time } = bookingData;
+  const barbers = await fetchBarbers();
 
+  const { email, barberId, date, time } = bookingData;
   if (!email || !email.includes('@')) throw new Error("Invalid email");
   if (!barberId) throw new Error("Invalid barberId");
   if (!date || !time) throw new Error("Invalid datetime");
 
+  const barber = barbers.find((barber) => barber.id === barberId);
+  if (!barber) throw new Error("Barber not found");
+
   const bookingDate = new Date(`${date}T${time}`);
-  if (isNaN(bookingDate.getTime())) throw new Error("Invalid datetime");
-  if (bookingDate <= new Date()) throw new Error("Past date");
   const bookingConflict = bookings.some((booking) => booking.barberId === barberId && booking.date === date && booking.time === time);
 
+  if (isNaN(bookingDate.getTime())) throw new Error("Invalid datetime");
+  if (bookingDate <= new Date()) throw new Error("Past date");
   if (bookingConflict) throw new Error("Time slot already booked");
+
+  const dayIndex = bookingDate.getDay();
+  const dayName = WEEKDAYS[dayIndex];
+  const workSchedule = barber.workSchedule[dayName];
+
+  if (!workSchedule) throw new Error("Barber does not work on this day");
+  if (time < workSchedule.start || time >= workSchedule.end) throw new Error("Time outside of barber's working hours");
 
   const newBooking: Booking = {
     id: randomUUID(),
