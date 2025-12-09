ecb15a9f6af8d82e16b24f6bc58dcc30923811f7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-09 10:16:09 +0100

    insert routers, separate index.ts and server.ts for better architecture

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,4 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import dotenv from "dotenv";
-import { authenticate } from "./middlewares/auth.ts";
-import barbersRouter from "./routes/barbers.ts";
-
-dotenv.config();
-
-const app = express();
-
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "ok",
-    timestamp: new Date().toISOString(),
-    service: "nn-coding-challenge-backend"
-  })
-})
-
-app.use(authenticate);
-
-app.use("/api/barbers", barbersRouter);
+import app from "./server.ts";
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server running on port ${process.env.PORT || 3001}`);
---
+++ backend/src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import { authenticate } from "./middlewares/auth.ts";
+import barbersRouter from "./routes/barbers.ts";
+import bookingsRouter from "./routes/bookings.ts";
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    timestamp: new Date().toISOString(),
+    service: "nn-coding-challenge-backend"
+  })
+})
+
+app.use(authenticate);
+
+app.use("/api/barbers", barbersRouter);
+app.use("/api/booking", bookingsRouter);
+
+export default app;
