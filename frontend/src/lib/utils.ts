4d36a3e85d9e4a9d1c98028105515a34ea8e890a
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-11 11:38:39 +0100

    Add timeslot generate util function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateSlots(openTime: number, closeTime: number) {
+  const slots: string[] = [];
+  for (let hour = openTime; hour < closeTime; hour++) {
+    slots.push(`${hour.toString().padStart(2, "0")}:00`);
+  }
+  return slots;
+}
