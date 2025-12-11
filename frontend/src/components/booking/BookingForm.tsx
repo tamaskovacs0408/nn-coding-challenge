99d581ebaa5a7e8322d84f39c935b46c1064025d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-11 11:36:08 +0100

    Refactor BookingForm

--- frontend/src/components/booking/BookingForm.tsx
+++ frontend/src/components/booking/BookingForm.tsx
@@ -1,40 +1,65 @@
 "use client";
 
-import { useActionState } from "react";
+import { useActionState, useState, useEffect } from "react";
 import { createBookingAction } from "@/app/actions/createBooking";
+import { useRouter } from "next/navigation";
 import BookingFormField from "./BookingFormField";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import type { Barber } from "@/types/barber";
 import type { BookingFormState } from "@/types/form";
 import "./BookingForm.scss";
+import BookingDateTimePicker from "./BookingDateTimePicker";
 
 const initialState: BookingFormState = {
   success: false,
   error: null,
 };
 
-export default function BookingForm({ barber }: { barber: Barber}) {
-  const [ state, formAction ] = useActionState(createBookingAction, initialState);
+export default function BookingForm({ barber }: { barber: Barber }) {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  const [state, formAction] = useActionState(createBookingAction, initialState);
+
+  const router = useRouter();
 
   if (state.success) {
     toast.success("Sikieres foglalás!");
+
+    setTimeout(() => {
+      router.push("/bookings");
+    }, 1000);
   }
 
   if (state.error) {
-    toast.error("Hibe történt a foglalás során.");
+    toast.error("Hiba történt a foglalás során.");
     console.error(state.error);
   }
 
   return (
-    <form action={formAction} className="booking-form">
-      <input type="hidden" name="barberId" value={barber.id} />
+    <form action={formAction} className='booking-form'>
+      <input type='hidden' name='barberId' value={barber.id} />
+      <input type='hidden' name='date' value={date} />
+      <input type='hidden' name='time' value={time} />
 
-      <BookingFormField labelName="E-mail" inputType="email" inputName="email" />
-      <BookingFormField labelName="Dátum" inputType="date" inputName="date" />
-      <BookingFormField labelName="Időpont" inputType="time" inputName="time" />
+      <BookingFormField
+        labelName='E-mail'
+        inputType='email'
+        inputName='email'
+      />
 
-      <Button type="submit" className="booking-form__submit">Foglalás</Button>
+      <BookingDateTimePicker
+        barberId={barber.id}
+        onSelect={(selectedDate, selectedTime) => {
+          setDate(selectedDate);
+          setTime(selectedTime);
+        }}
+      />
+
+      <Button type='submit' className='booking-form__submit'>
+        Foglalás
+      </Button>
     </form>
-  )
+  );
 }
