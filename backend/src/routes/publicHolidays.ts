315262344b5ed4cc78dc73433959bd9d30a9f11e
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-19 17:48:52 +0100

    Create route for public holidays

---
+++ backend/src/routes/publicHolidays.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import fetchPublicHolidays from "../services/publicHolidayService.ts";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const { year } = req.query;
+
+  if (year) {
+    const publicHolidays = await fetchPublicHolidays(year as string);
+
+    return res.status(200).json(publicHolidays);
+  }
+
+  return res.status(400).json({ message: "Year required"});
+})
+
+export default router;
