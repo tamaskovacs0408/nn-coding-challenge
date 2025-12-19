45219d24a9b56fb42d193f8ba003c6eb43d06f40
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-19 17:48:16 +0100

    Create service for public holidays

---
+++ backend/src/services/publicHolidayService.ts
@@ -0,0 +1,12 @@
+import type { PublicHolidays } from "../types/publicHolidays.ts";
+import { apiFetch } from "../lib/utils.ts";
+
+async function fetchPublicHolidays(year: string): Promise<PublicHolidays[]> {
+  const url = process.env.PUBLIC_HOLIDAY_API_URL!;
+
+  const publicHolidays: PublicHolidays[] = await apiFetch(`${url}/${year}/HU`);
+
+  return publicHolidays;
+}
+
+export default fetchPublicHolidays;
