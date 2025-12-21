25627fb137274b84540146f403e0d8b4efb7bb8c
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-21 17:10:52 +0100

    Install db related packages, create drizzle config

---
+++ backend/drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'drizzle-kit'
+import { config } from 'dotenv'
+config()
+
+export default {
+  schema: './src/db/schema',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config
