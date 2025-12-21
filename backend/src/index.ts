f976a1127eaea34a987872fdcd67ea342fccdbd7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-21 17:28:13 +0100

    add caching function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
 import app from "./server.js";
+import { cacheBarbers } from "./services/barbers.cache.js";
+
+await cacheBarbers();
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server running on port ${process.env.PORT || 3001}`);
