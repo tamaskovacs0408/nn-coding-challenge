66a616c775a2ccfb832fc78f2da49ca65b24618b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-12-21 17:27:47 +0100

    Update service with db data

--- backend/src/services/barberService.ts
+++ backend/src/services/barberService.ts
@@ -1,23 +1,5 @@
-import type { Barber } from "../types/barbers.ts";
+import { getCachedBarbers } from "./barbers.cache.js";
 
-async function fetchBarbers(): Promise<Barber[]> {
-  const url = process.env.BARBER_API_URL!;
-  const key = process.env.BARBER_API_KEY!;
-
-  const response = await fetch(url,
-    {
-      headers: {
-        "x-api-key": key
-      }
-  })
-
-  if (!response.ok) {
-    throw new Error(`HTTP error, status: ${response.status}`);
-  }
-
-  const data = await response.json();
-
-  return data;
+export default async function getBarbers() {
+  return getCachedBarbers();
 }
-
-export default fetchBarbers
